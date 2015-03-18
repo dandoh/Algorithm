fcb1b18cf5db25a48415ade49dc52d6b469aea53
Dandoh <dainhan605@gmail.com>
2015-03-18 20:07:39 +0700

    :wq :wq

---
+++ Week 7/src/Graph.java
@@ -0,0 +1,33 @@
+
+
+public class Graph {
+	private int V;
+	private int E;
+	private Bag<Integer>[] adj;
+	
+	@SuppressWarnings("unchecked")
+	public Graph(int V) {
+		this.V = V;
+		this.E = 0;
+		adj = (Bag<Integer>[]) new Bag[V];
+		for (int i = 0; i < V; i++) {
+			adj[i] = new Bag<Integer>();
+		}
+	}
+	
+	public Graph(In in) {
+		this(in.readInt());
+		int E = in.readInt();
+		for (int i = 0; i < E; i++) {
+			int v = in.readInt();
+			int w = in.readInt();
+			addEdge(v, w);
+		}
+	}
+	
+	public void addEdge(int v, int w) {
+		adj[v].add(w);
+		adj[w].add(v);
+		E++;
+	}
+}
